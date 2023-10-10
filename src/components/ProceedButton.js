e0bd0123f46c6254523d364d0f1c238dbb984275
Priya Mane <90308766+priyamane2019@users.noreply.github.com>
2023-10-11 02:55:50 +0530

    Updated ProceedButton

--- src/components/ProceedButton.js
+++ src/components/ProceedButton.js
@@ -7,7 +7,7 @@ const ProceedBooking = (props) => {
   const handleProceed = () => {
 
     if (props.selectedSeats.length == props.SelectTicketQuantity) {
-      // props.setProceedClicked(true);
+      
       setConfirmationVisible(true);
 
     }
@@ -19,16 +19,6 @@ const ProceedBooking = (props) => {
     }
 
 
-    // if (props.selectedSeats.length == 0)
-    // {
-    //   alert("Please select The Tickets")
-    //   setConfirmationVisible(false);
-
-    // }else{
-    //   setConfirmationVisible(true);
-
-    // }
-    // props.setProceedClicked(true);
 
   }
   const confirmBooking = () => {
@@ -43,10 +33,10 @@ const ProceedBooking = (props) => {
 
 
   const cancelBooking = () => {
-    // Hide the confirmation popup when the user cancels
+    
     setConfirmationVisible(false);
 
-    window.location.reload(); // Refresh the page
+    window.location.reload(); 
 
 
   };
@@ -72,62 +62,3 @@ export default ProceedBooking
 
 
 
-
-
-/*import React, { useState } from 'react';
-
-function App() {
-  const [selectedSeats, setSelectedSeats] = useState(0);
-  const [isConfirmationVisible, setConfirmationVisible] = useState(false);
-
-  const handleSeatClick = () => {
-    // Implement your logic here to handle seat selection
-    // For this example, we'll just increase the selected seats count by 1
-    setSelectedSeats(selectedSeats + 1);
-  };
-
-  const handleProceedClick = () => {
-    // Show the confirmation popup when the "Proceed" button is clicked
-    setConfirmationVisible(true);
-  };
-
-  const confirmBooking = () => {
-    // Implement your logic to confirm the booking here
-    // For this example, we'll just hide the confirmation popup
-    setConfirmationVisible(false);
-    alert('Booking confirmed!'); // You can replace this with your own logic.
-  };
-
-  const cancelBooking = () => {
-    // Hide the confirmation popup when the user cancels
-    setConfirmationVisible(false);
-  };
-
-  return (
-    <div>
-      <h1>Select Seats</h1>
-      <p>Selected Seats: {selectedSeats}</p>
-
-      {/* Create a number of seat divs, and add onClick handler */
-/*
-<div className="seat" onClick={handleSeatClick}></div>
-<div className="seat" onClick={handleSeatClick}></div>
-<div className="seat" onClick={handleSeatClick}></div>
-{/* Add more seat divs as needed */
-
-/*<button onClick={handleProceedClick}>Proceed</button>
-
-      {/* Show the confirmation popup conditionally */
-/*{isConfirmationVisible && (
-        <div className="confirmation-popup">
-          <h2>Confirmation</h2>
-          <p>Do you want to confirm your booking for {selectedSeats} seats?</p>
-          <button onClick={confirmBooking}>Yes</button>
-          <button onClick={cancelBooking}>No</button>
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default App;*/
